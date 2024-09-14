07d23d7297e3c11078cc1b455df6467a37f78713
Khavin K K <khavinkrishnan@gmail.com>
2024-09-14 15:52:43 -0400

    added useAuthInfo hook

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,6 +1,8 @@
 import { Switch, Route } from "wouter";
-
+import { useAuthInfo } from "@propelauth/react";
 export function Dashboard() {
+  const info = useAuthInfo();
+  console.log(info);
   return (
     <div>
       <Switch>
